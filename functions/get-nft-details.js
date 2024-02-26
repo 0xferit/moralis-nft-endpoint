640eaa12bbef9ac87bbf89e1fa968fc6c3d56057
Ferit <ferit@cryptolab.net>
2024-02-26 18:32:03 +0000

    fix body value

--- functions/get-nft-details.js
+++ functions/get-nft-details.js
@@ -22,7 +22,7 @@ exports.handler = async (event, context) => {
 
         return {
             statusCode: 200,
-            body: data,
+            body: JSON.stringify(data),
             headers: {
                 'Content-Type': 'application/json',
             },
